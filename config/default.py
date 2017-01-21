a753a656d65fd3580dbe0c2032f3dbbbf4cc7177
Élie Deloumeau <elie.deloumeau@fr.clara.net>
2017-01-21 19:26:51 +0100

    add ssl support

--- config/default.py
+++ config/default.py
@@ -2,6 +2,12 @@
 # -*- coding: utf-8 -*-
 from datetime import timedelta
 
+SSL = False
+# Easy way : make-ssl-cert generate-default-snakeoil --force-overwrite
+SSL_CERT = '/etc/ssl/certs/ssl-cert-snakeoil.pem'
+SSL_KEY = '/etc/ssl/private/ssl-cert-snakeoil.key'
+
+
 DEBUG = False
 SECRET_KEY = u'ça cest une vrai appli ! il faut que la clé soit bien longue'
 SQLALCHEMY_DATABASE_URI = 'sqlite:///../lxc-webpanel.sqlite'
--- run.py
+++ run.py
@@ -6,11 +6,18 @@ try:
     host = app.config['HOST']
 except KeyError:
     host = '127.0.0.1'
-    
 
 try:
     port = app.config['PORT']
 except KeyError:
     port = 5000
 
-app.run(host=host, port=port, threaded=True)
+try:
+    ssl = app.config['SSL']
+except KeyError:
+    ssl = False
+
+if ssl:
+	app.run(host=host, port=port, threaded=True, ssl_context=(app.config['SSL_CERT'], app.config['SSL_KEY']))
+else:
+	app.run(host=host, port=port, threaded=True)
