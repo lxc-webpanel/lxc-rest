3e0de717ba6935caa5325439142810f38b1bb47f
Élie Deloumeau <elie.deloumeau@fr.clara.net>
2017-07-03 23:58:01 +0200

    try to fix cpu percent

--- app/views.py
+++ app/views.py
@@ -1102,7 +1102,7 @@ class HostStats(Resource):
 
         cpu_count_logical = psutil.cpu_count()
         cpu_count_physical = psutil.cpu_count(logical=False)
-        cpu_percent = psutil.cpu_percent(interval=0.1)
+        cpu_percent = psutil.cpu_percent(interval=0.2)
 
         virtual_memory = psutil.virtual_memory()
         swap_memory = psutil.swap_memory()
